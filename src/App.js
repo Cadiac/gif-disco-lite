31600ebe530e755f370b10a1dca14db1bc129fda
Jaakko Husso <jaakko.husso@futurice.com>
2017-03-18 00:15:06 +0200

    Connect Seriously effect settings to React state.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Seriously from 'seriously';
-import 'seriously/effects/seriously.vignette.js';
+import 'seriously/effects/seriously.vignette';
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,8 +9,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      amount: 10,
+      amount: 1,
     };
+
+    this.handleChange = this.handleChange.bind(this);
 
     this.seriously = new Seriously();
     this.vignette = this.seriously.effect('vignette');
@@ -30,12 +32,30 @@ class App extends Component {
     this.seriously.go();
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    // Connect Seriously effect settings to state
+    this.vignette.amount = nextState.amount;
+  }
+
+  handleChange(event) {
+    this.setState({ amount: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <input
+            type="range"
+            id="vignette-range"
+            min="0"
+            max="20"
+            step="0.0001"
+            value={this.state.amount}
+            onChange={this.handleChange}
+          />
         </div>
         <img src="images/colorbars.png" id="colorbars" alt="colorbars" />
         <canvas id="canvas" width="640" height="480" />
