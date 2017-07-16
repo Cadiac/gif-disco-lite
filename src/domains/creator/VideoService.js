61f536b64fb08c61245c6eade594982dedec2aa6
Jaakko Husso <jaakko.husso@futurice.com>
2017-07-16 22:43:01 +0300

    Connect gif recording to render events

--- src/domains/creator/VideoService.js
+++ src/domains/creator/VideoService.js
@@ -29,11 +29,16 @@ export default class VideoService {
     this.reformat.mode = 'cover';
     this.reformat.width = 480;
     this.reformat.height = 480;
+
+    this.recording = false;
+
+    this.addCurrentFrame = this.addCurrentFrame.bind(this);
   }
 
   setCompositionTargets(canvas) {
     // Composition target
     this.target = this.composition.target(canvas);
+
     // TODO: Get rid of this
     this.canvas = canvas;
   }
@@ -58,25 +63,31 @@ export default class VideoService {
     this.target.source = this.split;
 
     console.log('Starting video composition!');
+
     // Start the composition
     this.composition.go();
   }
 
+  addCurrentFrame() {
+    this.gif.addFrame(this.canvas, { delay: 33, copy: true });
+  }
+
   startRecording() {
+    console.log('Started gif generation.');
+
+    this.recording = true;
+
     this.gif = new GIF({
       workers: 2,
       quality: 10,
       transparent: 'rgba(0, 0, 0, 0)',
     });
 
-    console.log('Started gif generation.');
-    this.interval = setInterval(() => {
-      this.gif.addFrame(this.canvas, { delay: 100, copy: true });
-    }, 100);
+    this.target.on('render', this.addCurrentFrame);
   }
 
   stopRecording(onCreate) {
-    clearInterval(this.interval);
+    this.target.off('render', this.addCurrentFrame);
 
     this.gif.on('finished', (blob) => {
       console.log('Gif generation finished!');
